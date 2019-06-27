c40326f54dfa4ffef62c4e5afdca370d5c40b2a4
Phil Kunz <gitlab@philkunz.com>
2019-06-27 14:00:24 +0200

    fix(core): update

--- ts/mod_csv/mod.paths.ts
+++ ts/mod_csv/mod.paths.ts
@@ -2,6 +2,6 @@ import * as plugins from './mod.plugins';
 import * as paths from '../paths';
 export * from '../paths';
 
-export const csvDir = paths.nogitDir;
+export const csvDir = plugins.path.join(paths.nogitDir, 'gdrive');
 export const csvInputDir = plugins.path.join(csvDir, 'input');
 export const csvOutputDir = plugins.path.join(csvDir, 'output');
